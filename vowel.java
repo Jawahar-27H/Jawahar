9cf24efd97b3aacfe94dbbed421c62f3da5f955c
Jawahar-27H <jawaharvjksr@gmail.com>
2017-06-19 14:57:53 +0530

    Add files via upload

---
+++ vowel.java
@@ -0,0 +1,9 @@
+class vowconst{
+public static void main(String[] args){
+char ch=args[0].charAt(0);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+System.out.println("character is vowel");
+else
+System.out.println("character is consonanat");
+}
+}
