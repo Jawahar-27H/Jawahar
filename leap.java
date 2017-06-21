b3bf410b1f5fcaf9e3a3ccfa56af62f8f8f6c138
Jawahar-27H <jawaharvjksr@gmail.com>
2017-06-21 09:40:16 +0530

    Add files via upload

---
+++ leap.java
@@ -0,0 +1,9 @@
+class leap{
+public static void main(String[] args){
+int a=Integer.parseInt(args[0]);
+if(a%4==0)
+System.out.println("Leap year"+a);
+else
+System.out.println("Not a Leap year"+a);
+}
+}
