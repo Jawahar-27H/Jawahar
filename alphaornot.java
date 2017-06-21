e8eae1d034b9885db6d80c4706e6bd4f0f09d211
Jawahar-27H <jawaharvjksr@gmail.com>
2017-06-21 09:18:18 +0530

    Add files via upload

---
+++ alphaornot.java
@@ -0,0 +1,9 @@
+class alphabet{
+public static void main{
+char ch=args[0]charAt(0);
+if((ch>='a')&&(ch<='z'))||((ch>='A')&&(ch<='Z'))
+System.out.println("It is an Alphabet");
+else
+System.out.println("It is not an Alphabet");
+}
+}
