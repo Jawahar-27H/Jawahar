defa53fe11328e067fa1bed9b7afd9a335d87ccb
Jawahar-27H <jawaharvjksr@gmail.com>
2017-07-14 11:59:51 +0530

    Update size.java

--- size.java
+++ size.java
@@ -1,23 +1,23 @@
 #include<stdio.h>
 #include<conio.h>
-int a(const void*d,const void*n)
+int a(const void*d,const void*s)
 {
-return(*(char*)n)-(*(char*d));
+return(*(char*)s)-(*(char*d));
 }
 void main()
 {
-int i,j,n;
-char n[2*10];
+int i,j,s;
+char s[2*10];
 scanf("%d",&a);
 j=a;
 while(i<a)
 {
 i=0;
-n[i]='B';
-n[j+1]='W';
+s[i]='B';
+s[j+1]='W';
 j++;
 }
-n[a*2]='\0';
+s[a*2]='\0';
 qsort(n,2*a,sizeof(char),a);
-printf("%s",n);
+printf("%s",s);
 }
